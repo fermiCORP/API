71d52455017a958e3753a2701a8c0764d5496639
Maury6000 <gentilemaurizio6@gmail.com>
2024-01-28 00:23:06 +0100

    funziona

--- api.js
+++ api.js
@@ -1,16 +1,38 @@
-const axios = require('axios');
-
-const apiKey = 'a89cc6541957497284c22834dfaa49e3'; // Sostituisci con la tua chiave API da RAWG
+const apiKey = 'a89cc6541957497284c22834dfaa49e3';
 const theme = 'action';
 const limit = 10;
 
 const apiUrl = `https://api.rawg.io/api/games?key=${apiKey}&genres=${theme}&page_size=${limit}`;
 
+function createGameListItem(game) {
+    const listItem = document.createElement('li');
+    const image = document.createElement('img');
+
+    image.src = game.background_image;
+    image.alt = game.name;
+
+    const details = document.createElement('div');
+    details.className = 'details';
+    details.innerHTML = `<strong>${game.name}</strong><br>Punteggio: ${game.rating}<br>Data di rilascio: ${game.released}`;
+
+    listItem.appendChild(image);
+    listItem.appendChild(details);
+
+    return listItem;
+}
+
+function displayGames(games) {
+    const gameList = document.getElementById('gameList');
+    games.forEach(game => {
+        const listItem = createGameListItem(game);
+        gameList.appendChild(listItem);
+    });
+}
+
 axios.get(apiUrl)
     .then(response => {
-        // Gestisci la risposta qui
         const games = response.data.results;
-        console.log(`Primi ${limit} giochi con il tema ${theme}:`, games);
+        displayGames(games);
     })
     .catch(error => {
         console.error('Errore nella richiesta API:', error);
